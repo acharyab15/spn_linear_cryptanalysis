8f91963ef1b7b851f8938a7984a5d3019c7de24f
acharyab15 <acharyab15@mail.wlu.edu>
2015-04-18 02:20:51 -0400

    Update linearCryptanalysis.py
    This framework provides an open-source Python Implementation of Linear Cryptanalysis of a Substitution-Permutation Network cipher. It is a step towards automating the process of linear cryptanalysis by automatic generation of linear approximations and linear expressions to help in cryptanalysis.

--- linearCryptanalysis.py
+++ linearCryptanalysis.py
@@ -1,3 +1,15 @@
+'''
+linearCryptanalysis.py
+
+This framework is an implementation of linear cryptanalysis of a substitution-permutation network.
+
+This is an open source software and can be redistributed or modified as deemed appropriate.
+
+Credits: https://github.com/keithmgould/cryptanalysis.git
+
+Copyright (C) 2015 Bipeen Acharya
+
+'''
 SBOXFORWARD =  [14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7]
 SBOXBACKWARD = [14,3,4,8,1,12,10,15,7,13,9,6,11,2,0,5]
 PERMUTATION =  [0,4,8,12,1,5,9,13,2,6,10,14,3,7,11,15]
@@ -178,6 +190,7 @@ def automatePaths(L):
     
     print(Pstring + " + " + Cstring + " = 0")
 
+#Validate the paths for linear cryptanalysis
 def validatePaths(L):
     for i in range(len(L)-1):
         for j in range(len(L[i])):
@@ -191,6 +204,7 @@ def validatePaths(L):
                 return "Invalid substitution"  
     return "Valid Path"
 
+#Generate random approximations
 def generatePaths():
     L= [[[0 for i in range(8)] for j in range(4)] for j in range(4)]
     for j in range(4):
